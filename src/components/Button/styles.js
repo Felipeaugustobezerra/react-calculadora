2bebfee2d43d456f75a5488de1c34023698979c6
Felipeaugustobezerra <58082993+Felipeaugustobezerra@users.noreply.github.com>
2024-02-24 21:19:39 +0000

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding:30px;
+    border:1px solid #CDCDCD;
+    background-color:#303136;
+    color:#FFFFFF;
+    cursor:pointer;
+    font-size:20px;
+    font-weight:700;
+    border-radius:50%;
+    border:none;
+    margin:8px;
+    flex:1;
+
+
+    &:hover{
+        opacity:0.5
+    }
+    
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width : 100%;
+    height: 100px;
+    background-color:#00000;
+
+    display:flex;
+    align-items:center;
+    justify-content:flex-end;
+
+    font-size:24px;
+    font-family:'Roboto';
+    margin-bottom:20px;
+    
+    
+
+    input{
+        width:100%;
+        height: 100px;
+        background-color:#000000;
+        border: 0;
+        padding: 0 10px;
+        border:1px solid #FFFFFF;
+
+        font-size:35px;
+        font-family:'Roboto';
+        color:#FFFFFF
+        
+    }
+`
